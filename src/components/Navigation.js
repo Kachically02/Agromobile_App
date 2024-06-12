541fdc40196b4ceb4b57c9e05960c307f12b2265
kachically02 <kachically89@gmail.com>
2024-06-12 11:12:41 +0100

    First commit for my app

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../images/logo.svg";
+
+function Navigation() {
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
