585709de2f7cda42cebf5a02115c4350a2c9885f
kachically02 <kachically89@gmail.com>
2024-06-22 23:10:36 +0100

    Update on the footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {FaLinkedin, FaFacebookF, FaYoutube, FaInstagram } from "react-icons/fa"
+import { BsTwitterX } from "react-icons/bs";
 import "./footer.css"
 
 function Footer() {
@@ -9,33 +11,28 @@ function Footer() {
           <div className="row">
             <div className="col-12 col-md-4">
             <h5>Agromobile</h5>
-              <ul>
-                <l1>About Us</l1>
-                <l1>Blog</l1>
-                <l1>Career</l1>
-                <l1>Suppliers</l1>
-                <l1>Food Safety</l1>
-              </ul>
+                <span>About Us</span>
+                <span>Blog</span>
+                <span>Career</span>
+                <span>Suppliers</span>
+                <span>Food Safety</span>
+            </div>
+            <div className="col-12 col-md-4">
+            <h5>Help</h5>
+            <span>Contact Us</span>
+            <span>Delivery Information</span>
+            <span>Support</span>
+            <span>FAQs</span>
+            <span>Partners</span>
+            <span>Terms Of Service</span>
             </div>
             <div className="col-12 col-md-4">
             <h5>Agromobile</h5>
-              <ul>
-                <l1>About Us</l1>
-                <l1>Blog</l1>
-                <l1>Career</l1>
-                <l1>Suppliers</l1>
-                <l1>Food Safety</l1>
-              </ul>
-            </div>
-            <div className="col-12 col-md-4">
-            <h5>Agromobile</h5>
-              <ul>
-                <l1>About Us</l1>
-                <l1>Blog</l1>
-                <l1>Career</l1>
-                <l1>Suppliers</l1>
-                <l1>Food Safety</l1>
-              </ul>
+                <span className="icons"><FaLinkedin />  LinkedIn</span>
+                <span className="icons"><BsTwitterX />  Twitter</span>
+                <span className="icons"><FaFacebookF />  Facebook</span>
+                <span className="icons"><FaYoutube />  Youtube</span>
+                <span className="icons"><FaInstagram />  Instagram</span>
             </div>
             <hr/>
             <p>&copy; Agromobile 2024, All Rights Reserved.</p>
